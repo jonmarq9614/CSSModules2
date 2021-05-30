0d655841063ff8ce336da298fb18a48bfb3cd44f
Jonathan Marquez <jmarqu12@icloud.com>
2021-05-30 01:19:56 -0600

    midterm

--- MyGramCSS/Controllers/HomeController.cs
+++ MyGramCSS/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace MyGramCSS.Controllers
             return View();
         }
 
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
